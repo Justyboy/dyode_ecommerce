2921f1851bf0e51032d7679553dfea98e1c44582
Justin <justinl@starpilwax.com>
2022-03-02 19:56:00 -0500

    save header component added

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import Header from './components/Header';
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+  <Header />
+
+ 
+     {/* Banner */}
+    {/* Collections */}
+    {/* Product Slider */}
+    {/* Bottom Banner */}
+    {/* Newsletter */}
+    {/* Footer */}
+   </div>
+
+
+  );
+}
+
+export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,37 @@
-import React from 'react'
+
+import SearchIcon from '@mui/icons-material/Search';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+
 
 function Header() {
   return (
-    <div>Header</div>
+      <div className='header'>
+    <div className="header__left">
+   <img src='https://d1csarkz8obe9u.cloudfront.net/posterpreviews/creative-logo-or-icon-design-template-d6cdd0b35fc4b69ddc3288a7abd52ff3_screen.jpg?ts=1578260767' 
+   alt="Company logo"
+   width="100px"
+   height="100px"
+   /> 
+   </div>
+   <div className='header__center'>
+     <ul>
+       <li>HOME</li>
+       <li>WOMEN'S</li>
+       <li>ACCESSORIES</li>
+       <li>SALE</li>
+     </ul>
+     </div>
+   <div className='header__right'>
+       
+      <SearchIcon />
+      <AccountBoxIcon/
+      <ShoppingCartIcon />  
+
+
+   </div>
+   </div>
   )
 }
 
